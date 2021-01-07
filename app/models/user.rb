89fa9a34bcb516fb4bae2f86b64c0f311def264f
sochi0720 <so-js.share_b@docomo.ne.jp>
2021-01-07 12:45:43 +0900

    新規登録/ユーザー情報確認、本人情報確認実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nick_name, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true, format: { with: /^[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /^[ァ-ンー－]+$/, message: '全角カタカナを使用してください' } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+
+  with_options presence: true, format: { with: /^[a-zA-Z0-9]+$/, message: '半角英数字を使用してください' } do
+    validates :encrypted_password, confirmation: true
+  end
+
   validates :birth_day, presence: true
+  validates :nick_name, presence: true
 end
