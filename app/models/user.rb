67d1d74001caf83ff8c57b6f4018be65484a237f
sochi0720 <so-js.share_b@docomo.ne.jp>
2021-01-11 12:24:37 +0900

    userモデルの単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true, format: { with: /^[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
     validates :family_name
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /^[ァ-ンー－]+$/, message: '全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ンー－]+\z/, message: '全角カタカナを使用してください' } do
     validates :family_name_kana
     validates :first_name_kana
   end
 
-  with_options presence: true, format: { with: /^[a-zA-Z0-9]+$/, message: '半角英数字を使用してください' } do
-    validates :encrypted_password, confirmation: true
+  with_options presence: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: '半角英数字を使用してください' } do
+    validates :password, confirmation: true
   end
 
   validates :birth_day, presence: true
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,95 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    it "nameとemail、passwordとpassword_confirmationが存在すれば登録できること" do
+      expect(@user).to be_valid
+    end
+
+    it "nick_nameが空では登録できないこと" do
+      @user.nick_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nick name can't be blank")
+    end
+
+    it "emailが空では登録できないこと" do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "passwordが空では登録できないこと" do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it "passwordが6文字以上であれば登録できること" do
+      @user.password = "123456"
+      @user.password_confirmation = "123456"
+      expect(@user).to be_valid
+    end
+
+    it "passwordが5文字以下であれば登録できないこと" do
+      @user.password = "12345"
+      @user.password_confirmation = "12345"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+
+    it "passwordが半角英数字混合でなければ登録できないこと" do
+      @user.password = "ＡＢＣＤＥＦ"
+      @user.password_confirmation = "ＡＢＣＤＥＦ"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password 半角英数字を使用してください")
+    end
+
+    it "passwordとpassword_confirmationが不一致では登録できないこと" do
+      @user.password = "123456"
+      @user.password_confirmation = "1234567"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it "生年月日が空では登録できないこと" do
+      @user.birth_day = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birth day can't be blank")
+    end
+
+    it "family_nameが全角入力でなければ登録できないこと" do
+      @user.family_name = "ｱｲｳｴｵ"
+      @user.valid?
+      expect(@user.errors[:family_name]).to include("全角文字を使用してください")
+    end
+
+    it "first_nameが全角入力でなければ登録できないこと" do
+      @user.first_name = "ｱｲｳｴｵ"
+      @user.valid?
+      expect(@user.errors[:first_name]).to include("全角文字を使用してください")
+    end
+
+    it "family_name_kanaが全角カタカナでなければ登録できないこと" do
+      @user.family_name_kana = "あいうえお"
+      @user.valid?
+      expect(@user.errors[:family_name_kana]).to include("全角カタカナを使用してください")
+    end
+
+    it "first_name_kanaが全角カタカナでなければ登録できないこと" do
+      @user.first_name_kana = "あいうえお"
+      @user.valid?
+      expect(@user.errors[:first_name_kana]).to include("全角カタカナを使用してください")
+    end
+  end
+end
